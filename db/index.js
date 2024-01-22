fdb764e671becea69d2711135cb58e0ce3418705
raggettii <22bsm029@iiitdmj.ac.in>
2024-01-22 13:55:54 +0530

    All files updated

--- db/index.js
+++ db/index.js
@@ -1,32 +1,39 @@
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://ijatinyadav:Gag0cYxkjHs2LrjE@cluster0.ndb9da0.mongodb.net/course-selling-webpage"
-).then = () => {
-  console.log("connected to the database");
-};
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String
 });
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
-const courseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: false },
-  price: { type: Number, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-  image: { type: String, required: false },
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
 });
 
-const Admin = mongoose.model("Admin", "adminSchema");
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
 
-const User = mongoose.model("User", "userSchema");
-
-const Courses = mongoose.model("Courses", "courseSchema");
-
-module.exports = { Admin, User, Courses };
+module.exports = {
+    Admin,
+    User,
+    Course
+}
